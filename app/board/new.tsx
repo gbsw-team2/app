31686ccec29da71974e81b642312240f67c92110
3suji3 <tnwl2799@gmail.com>
2025-04-02 09:16:36 +0900

    style: board style
    변경사항
    - 글쓰기 기본적인 style 설정

--- app/board/new.tsx
+++ app/board/new.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, SafeAreaView,StyleSheet } from "react-native";
 import { useState } from "react";
 
 const NewPostScreen = () => {
@@ -13,24 +13,49 @@ const NewPostScreen = () => {
   };
 
   return (
-    <View>
-      <Text>게시글 작성</Text>
-      <TextInput
-        placeholder="제목"
-        value={title}
-        onChangeText={setTitle}
-      />
-      <TextInput
-        placeholder="내용"
-        value={content}
-        onChangeText={setContent}
-        multiline
-      />
-      <TouchableOpacity onPress={handleSubmit}>
-        <Text>게시</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>글쓰기</Text>
+        <Text style={styles.label}>제목</Text>
+        <TextInput
+          value={title}
+          onChangeText={setTitle}
+        />
+        <Text>내용</Text>
+        <TextInput
+          value={content}
+          onChangeText={setContent}
+          multiline
+        />
+        <TouchableOpacity onPress={handleSubmit}>
+          <Text>게시</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default NewPostScreen;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingTop: 60,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#3E7BC9',
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  }
+})
