5e771c3a529ac7a11d77a1c0087cb234f38128e7
3suji3 <tnwl2799@gmail.com>
2025-04-08 00:40:41 +0900

    feat: start screen
    변경사항
    - 썸네일 화면 완성

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" />
         <Stack.Screen name="+not-found" />
         <Stack.Screen name="auth/signup" />
+        <Stack.Screen name="auth/login" />
         <Stack.Screen name="board/new" />
       </Stack>
       <StatusBar style="auto" />
