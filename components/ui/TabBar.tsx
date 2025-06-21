614e5298a4683af6618a8486d7e4284b8fd9ceff
choiseongwook11 <a4bingo3593@gmail.com>
2025-06-21 19:32:53 +0900

    feat: 텍스트번역 추가
    - 텍스트 번역을 추가했습니다

--- components/ui/TabBar.tsx
+++ components/ui/TabBar.tsx
@@ -2,13 +2,15 @@ import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { useRoute } from '@react-navigation/native';
 import { MaterialIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import { navigate } from "expo-router/build/global-state/routing";
+import { useState } from "react";
 
 const TabBar = () => {
   const router = useRouter();
   const route = useRoute();
   const isBoardRoute = route.name.startsWith('board');
   const isMyPageRoute = route.name.startsWith('mypage');
+  const isTranslateRoute = route.name.startsWith('translation')
+  const [showTranslateOptions, setShowTranslateOptions] = useState(false);
 
   return (
     <View style={styles.tabBar}>
@@ -21,12 +23,26 @@ const TabBar = () => {
         </View>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => navigate("/translation/voice")}>
+      <TouchableOpacity onPress={() => setShowTranslateOptions(!showTranslateOptions)}>
         <View style={styles.tabItem}>
-          <MaterialIcons name="translate" size={20} color="#989898" />
-          <Text style={styles.tabText}>번역</Text>
+          <MaterialIcons name="translate" size={20} color={isTranslateRoute ? "#3E7BC9" : "#989898"} />
+          <Text style={[styles.tabText, isTranslateRoute && styles.activeText]}>번역</Text>
         </View>
       </TouchableOpacity>
+
+      {showTranslateOptions && (
+        <View style={styles.dropdownContainer}>
+          <TouchableOpacity onPress={() => { router.push('/'); setShowTranslateOptions(false); }}>
+            <Text style={styles.option}>음성번역</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { router.push('/'); setShowTranslateOptions(false); }}>
+            <Text style={styles.option}>이미지번역</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { router.push('/translate/TextTranslate'); setShowTranslateOptions(false); }}>
+            <Text style={styles.option}>텍스트번역</Text>
+          </TouchableOpacity>
+        </View>
+      )}
 
       <TouchableOpacity onPress={() => router.push('/mypage/main')}>
         <View style={styles.tabItem}>
@@ -60,6 +76,27 @@ const styles = StyleSheet.create({
   activeText: {
     color: '#3E7BC9',
     fontWeight: '500',
+  },
+  dropdownContainer: {
+    position: 'absolute',
+    bottom: 70, 
+    left: '33%',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5, 
+    zIndex: 999, 
+  },
+
+  option: {
+    paddingVertical: 8,
+    fontSize: 16,
+    color: '#333',
   },
 });
 
